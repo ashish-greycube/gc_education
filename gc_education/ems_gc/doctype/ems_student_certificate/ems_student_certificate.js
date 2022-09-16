68e2f1f8e20a748121f103f3cded392f6974b1b7
vr-greycube <vijay@greycube.in>
2022-09-16 17:15:45 +0530

    feat: add ems student certificate doctype
    create print formats

---
+++ gc_education/ems_gc/doctype/ems_student_certificate/ems_student_certificate.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Greycube and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('EMS Student Certificate', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ gc_education/ems_gc/doctype/ems_student_certificate/test_ems_student_certificate.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, Greycube and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestEMSStudentCertificate(FrappeTestCase):
+	pass
