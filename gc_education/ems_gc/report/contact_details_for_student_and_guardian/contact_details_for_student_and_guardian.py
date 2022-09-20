86cab7cf993abb06569b66714ce18ba0ed13ad96
vr-greycube <vijay@greycube.in>
2022-09-20 19:28:29 +0530

    fix: contact details for guardian sql and new columns

--- gc_education/ems_gc/report/contact_details_for_student_and_guardian/contact_details_for_student_and_guardian.py
+++ gc_education/ems_gc/report/contact_details_for_student_and_guardian/contact_details_for_student_and_guardian.py
@@ -19,9 +19,14 @@ with fn as
 (
     select ROW_NUMBER() OVER(PARTITION BY parent ORDER BY tsg.idx) rn, 
     tsg.parent , tsg.guardian , tsg.guardian_name , tsg.relation ,
-    tg.mobile_number , tg.email_address , tg.alternate_number
+    tg.mobile_number , tg.email_address , tg.alternate_number ,
+    tg.annual_income , tg.occupation , tg.work_address , tg.name_of_firm ,
+    tg.user ,tg.date_of_birth ,tg.education 
     from `tabStudent Guardian` tsg
     inner join tabGuardian tg on tg.name = tsg.guardian 
+),
+intr as (
+    select parent , GROUP_CONCAT(interest) interests  from `tabGuardian Interest` tgi 
 )
     select 
         tpe.academic_year , tpe.academic_term , tpr.department ,
@@ -31,29 +36,45 @@ with fn as
         CONCAT_WS(', ', address_line_1, address_line_2, city, state ) address ,
         if(tsgs.active,'Active','Inactive') student_group_status ,
         if(ts.enabled,'Enabled','Disabled') student_status ,
-        fn.guardian_name guardian1_name,
-        fn.relation relation_with_guardian1, 
-        fn.mobile_number guardian1_mobile_no, 
-        fn.email_address guardian1_email_id, 
-        fn.alternate_number guardian1_alternate_no,
-        gr2.guardian_name guardian2_name,
-        gr2.relation relation_with_guardian2, 
-        gr2.mobile_number guardian2_mobile_no, 
-        gr2.email_address guardian2_email_id, 
-        gr2.alternate_number guardian2_alternate_no
+        fn.guardian_name  g1_guardian_name,
+        fn.education  g1_education,
+        fn.occupation  g1_occupation,
+        fn.work_address  g1_work_address,
+        fn.annual_income  g1_annual_income,
+        fn.name_of_firm  g1_name_of_firm,
+        fn.user  g1_user,
+        fn.date_of_birth  g1_date_of_birth,
+        fn.alternate_number  g1_alternate_number,
+        fn.mobile_number  g1_mobile_number,
+        fn.email_address  g1_email_address,
+        intr1.interests g1_interests,
+        fn2.guardian_name  g2_guardian_name,
+        fn2.education  g2_education,
+        fn2.occupation  g2_occupation,
+        fn2.work_address  g2_work_address,
+        fn2.annual_income  g2_annual_income,
+        fn2.name_of_firm  g2_name_of_firm,
+        fn2.user  g2_user,
+        fn2.date_of_birth  g2_date_of_birth,
+        fn2.alternate_number  g2_alternate_number,
+        fn2.mobile_number  g2_mobile_number,
+        fn2.email_address  g2_email_address,
+        intr2.interests g2_interests
     from tabStudent ts 
     inner join `tabProgram Enrollment` tpe on tpe.student = ts.name 
     inner join `tabProgram` tpr on tpr.name = tpe.program
     inner join `tabStudent Group` tsg on tsg.program = tpe.program and tsg.academic_term = tpe.academic_term 
     inner join `tabStudent Group Student` tsgs on tsgs.parent = tsg.name and tsgs.student = ts.name 
     left outer join fn on fn.parent = ts.name and fn.rn = 1
-    left outer join fn gr2 on gr2.parent = ts.name and gr2.rn = 2	
+    left outer join fn fn2 on fn2.parent = ts.name and fn2.rn = 2	
+    left outer join intr intr1 on intr1.parent = fn.guardian
+    left outer join intr intr2 on intr2.parent = fn2.guardian
     {conditions}
     order by tpe.program , tpe.student_batch_name , tsgs.group_roll_number , ts.g_r_number 
-        limit 20
     """.format(
             conditions=get_conditions(filters)
         ),
+        filters,
         as_dict=True,
     )
 
@@ -117,15 +138,29 @@ def get_columns():
         Student Mobile No,student_mobile_no,,,120 
         Student Email Id,student_email_id,,,120
         Student Address,address,,,120
-        Guardian1 Name,guardian1_name,,,120
-        Relation with Guardian1,relation_with_guardian1,,,120
-        Guardian1 Mobile No,guardian1_mobile_no,,,120
-        Guardian1 Email Id,guardian1_email_id,,,120
-        Guardian1 Alternate No,guardian1_alternate_no,,,120
-        Guardian2 Name,guardian2_name,,,120
-        Relation with Guardian2,relation_with_guardian2,,,120
-        Guardian2 Mobile No,guardian2_mobile_no, ,,120
-        Guardian2 Email Id,guardian2_email_id,,,120
-        Guardian2 Alternate No,guardian2_alternate_no,,,120
+        Guardian1 Name,g1_guardian_name,,,120
+        Guardian1 Mobile No,g1_mobile_number,,,120
+        Guardian1 Email,g1_email_address,,,120
+        Guardian1 Alt Number,g1_alternate_number,,,120
+        Guardian1 Education,g1_education,,,120
+        Guardian1 Occupation,g1_occupation,,,120
+        Guardian1 Work Address,g1_work_address,,,120
+        Guardian1 Annual Income,g1_annual_income,,,120
+        Guardian1 Firm,g1_name_of_firm,,,120
+        Guardian1 User,g1_user,,,120
+        Guardian1 DoB,g1_date_of_birth,,,120        
+        Guardian1 Interests,g1_interests,,,120
+        Guardian2 Name,g2_guardian_name,,,120
+        Guardian2 Mobile No,g2_mobile_number,,,120
+        Guardian2 Email,g2_email_address,,,120
+        Guardian2 Alt Number,g2_alternate_number,,,120
+        Guardian2 Education,g2_education,,,120
+        Guardian2 Occupation,g2_occupation,,,120
+        Guardian2 Work Address,g2_work_address,,,120
+        Guardian2 Annual Income,g2_annual_income,,,120
+        Guardian2 Firm,g2_name_of_firm,,,120
+        Guardian2 User,g2_user,,,120
+        Guardian2 DoB,g2_date_of_birth,,,120
+        Guardian2 Interests,g2_interests,,,120
     """
     )
